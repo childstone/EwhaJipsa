f60088f90d001a77c1231578a4f6787b3c071d0e
Kminseo <childstone@users.noreply.github.com>
2024-10-09 17:23:28 +0900

    Revert "Merge branch 'main' into Hyun"
    This reverts commit f55131f757346bb10c3a7dbd6678287d157e43a7, reversing
    changes made to f5f1b6c5831f47a4100dbae79bad13b8d3e267ac.

--- Jipsa_YearEndParty/Assets/Script/ChScene1.cs
+++ Jipsa_YearEndParty/Assets/Script/ChScene1.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Clothes : MonoBehaviour
+public class ChScene1 : MonoBehaviour
 {
-    [SerializeField]private int clothesSet;
 
-    public int GetClothesSet(){
-        return clothesSet;
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("Scene1");
     }
-
     // Start is called before the first frame update
     void Start()
     {
