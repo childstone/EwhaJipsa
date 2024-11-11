2852aefbb89d79573b6278b54f6a5724fc1f1d07
Kminseo <childstone@users.noreply.github.com>
2024-11-11 19:00:05 +0900

    Scene1 수정

--- Jipsa_YearEndParty/Assets/Script/ChScene2.cs
+++ Jipsa_YearEndParty/Assets/Script/ChScene2.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ChScene2 : MonoBehaviour
 {
+    public GameObject S2UI;
+    public Button btun1;
+    bool isUIActive = false;
 
-    public void SceneChange()
-    {
-        SceneManager.LoadScene("Scene2");
-    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        S2UI.SetActive(false);   
+    }
+
+    void OnClick(){
+        S2UI.SetActive(true);
+        isUIActive=true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        btun1.onClick.AddListener(OnClick);
+
+        if(isUIActive){
+            if(Input.GetMouseButtonDown(0))
+                S2UI.SetActive(false);   
+                
+        }
     }
 }
